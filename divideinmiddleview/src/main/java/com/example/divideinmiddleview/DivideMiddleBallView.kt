fb24798b16b8e5a666d0019366dfe0bbd379b580
Anwesh43 <anweshthecool0@gmail.com>
2020-11-03 02:55:10 +0530

    adding constants and libraries

--- divideinmiddleview/src/main/java/com/example/divideinmiddleview/DivideMiddleBallView.kt
+++ divideinmiddleview/src/main/java/com/example/divideinmiddleview/DivideMiddleBallView.kt
@@ -1,2 +1,26 @@
 package com.example.divideinmiddleview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#3F51B5",
+        "#FF9800",
+        "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val scGap : Float = 0.02f / parts
+val rFactor : Float = 5.6f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+
